b9746b8a19851dde127a83c2aeaf8131f829ccab
Tobias Palmer <tobiaspalmer93@gmail.com>
2018-05-20 18:04:57 +0200

    initial - blocks

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const {Nano} = require('nanode')
+const nano = new Nano({url: 'http://[::1]:7076'})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '!nanoblocks') {
+    nano.rpc('block_count').then((data) => {
+      msg.reply(data.count + ' blocks');
+    })
+  }
+});
+
+client.login(process.env.TOKEN);
