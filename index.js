a524240370f02b33100a196db114b7d185a24496
Tobias Palmer <tobiaspalmer93@gmail.com>
2018-11-01 20:16:37 +0100

    added calc

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
   nanoPrettify
 } = require('nano-prettify')
 var moment = require('moment');
+const Big = require('big.js');
 
 // client init
 
@@ -153,6 +154,80 @@ client.on('message', msg => {
 
     });
 
+  } else if (msgarray[0] === '.convert' || msgarray[0] === '.calc') {
+    if (typeof msgarray[1] === 'undefined') {
+      msg.reply('please add the amount!');
+      return;
+    }
+
+    // based on https://nanoo.tools/unit-converter
+
+    var inputBig = Big(msgarray[1]);
+
+    if (typeof msgarray[2] === 'undefined') {
+      var unit = msgarray[2];
+    } else {
+      var unit = 'NANO';
+    }
+
+    // define multipliers/dividers. dividers will be implemented as multipliers for precision reasons. 
+    multMnano = Big('1000000000000000000000000000000'); // 10^30
+    divMnano = Big('0.000000000000000000000000000001'); // 10^-30
+    multknano = Big('1000000000000000000000000000'); // 10^27
+    divknano = Big('0.000000000000000000000000001'); // 10^-27
+    multnano = Big('1000000000000000000000000'); // 10^24
+    divnano = Big('0.000000000000000000000001'); // 10^-24
+
+    // Convert input to raw amount
+    if (unit == "NANO") {
+      var raw = Big(inputBig.times(multMnano));
+    } else if (unit == "knano") {
+      var raw = Big(inputBig.times(multknano));
+    } else if (unit == "nano") {
+      var raw = Big(inputBig.times(multnano));
+    } else if (unit == "raw") {
+      var raw = Big(inputBig);
+    } else {
+      msg.reply('unit is not known!');
+      return;
+    }
+
+    msg.channel.send({
+      embed: {
+        color: 16007990,
+        fields: [{
+          name: "NANO",
+          value: Big(raw.times(divMnano)).toFixed().toString(),
+          inline: true
+        },
+        {
+          name: "knano",
+          value: Big(raw.times(divknano)).toFixed().toString(),
+          inline: true
+        },
+        {
+          name: "nano",
+          value: Big(raw.times(divnano)).toFixed().toString(),
+          inline: true
+        },
+        {
+          name: "raw",
+          value: raw.toFixed().toString(),
+          inline: true
+        },
+        {
+          name: "raw Hex",
+          value: pad16bytehex(dec2hex(raw.toFixed().toString())).toUpperCase(),
+          inline: true
+        }
+        ],
+        footer: {
+          icon_url: client.user.avatarURL,
+          text: 'My Nano Ninja'
+        }
+      }
+    });
+
   } else if (msg.content === '.ledger') {
     // ledger fastsync download
     msg.reply('https://mynano.ninja/api/ledger/download');
@@ -164,15 +239,15 @@ client.on('message', msg => {
   } else if (msg.content === '.wallet' || msg.content === '.wallets') {
     // trade talk
     msg.reply(
-      'unofficial wallet list:\n' + 
-      '**NanoVault:**\n' + 
-      '_Web, Windows, OSX, Linux_\n' + 
-      '<https://nanovault.io/> | <https://github.com/cronoh/nanovault/releases>\n\n' + 
-      '**Canoe:**\n' + 
-      '_Windows, OSX, Linux, iOS, Android_\n' + 
-      '<https://getcanoe.io/download/>\n\n' + 
-      '**Nano Wallet Company (NWC)**\n' + 
-      '_Windows, OSX, Linux, iOS, Android_\n' + 
+      'unofficial wallet list:\n' +
+      '**NanoVault:**\n' +
+      '_Web, Windows, OSX, Linux_\n' +
+      '<https://nanovault.io/> | <https://github.com/cronoh/nanovault/releases>\n\n' +
+      '**Canoe:**\n' +
+      '_Windows, OSX, Linux, iOS, Android_\n' +
+      '<https://getcanoe.io/download/>\n\n' +
+      '**Nano Wallet Company (NWC)**\n' +
+      '_Windows, OSX, Linux, iOS, Android_\n' +
       '<https://nanowalletcompany.com>'
     );
 
@@ -220,5 +295,38 @@ function round(value, precision) {
   }
 }
 
+// Zerofill HEX ... modified https://stackoverflow.com/questions/1267283/how-can-i-pad-a-value-with-leading-zeros
+function pad16bytehex(n) {
+  var pad = '00000000000000000000000000000000'
+  return (pad + n).slice(-pad.length);
+}
+
+function dec2hex(str, bytes = null) {
+  var dec = str.toString().split(''), sum = [], hex = [], i, s
+  while (dec.length) {
+    s = 1 * dec.shift()
+    for (i = 0; s || i < sum.length; i++) {
+      s += (sum[i] || 0) * 10
+      sum[i] = s % 16
+      s = (s - sum[i]) / 16
+    }
+  }
+  while (sum.length) {
+    hex.push(sum.pop().toString(16));
+  }
+
+  hex = hex.join('');
+
+  if (hex.length % 2 != 0)
+    hex = "0" + hex;
+
+  if (bytes > hex.length / 2) {
+    var diff = bytes - hex.length / 2;
+    for (var i = 0; i < diff; i++)
+      hex = "00" + hex;
+  }
+
+  return hex;
+}
 
 client.login(process.env.TOKEN);
