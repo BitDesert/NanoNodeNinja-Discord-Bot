71ff97b375ea61c54baee2a98715b9038524f145
BitDesert <tobiaspalmer93@gmail.com>
2020-06-30 12:03:00 +0200

    move web

--- index.js
+++ index.js
@@ -273,7 +273,7 @@ client.on('message', msg => {
       .addField('Natrium', '_Android, iOS_\n[Visit website](https://natrium.io)', true)
       .addField('Trust Wallet', '_Android, iOS_\n[Visit website](https://trustwallet.com/de/nano-wallet/)', true)
       .addField('Exodus', '_Windows, Mac, Linux, \nAndroid, iOS_\n[Visit website](https://www.exodus.io/)', true)
-      .addField('Guarda', '_Web, Windows, Mac, Linux, \nAndroid, iOS_\n[Visit website](https://guarda.com)', true)
+      .addField('Guarda', '_Windows, Mac, Linux, \nWeb, Android, iOS_\n[Visit website](https://guarda.com)', true)
       .addField('BrainBlocks', '_Web_\n[Visit website](https://app.brainblocks.io)', true)
       .addField('Magnum', '_Web, Android_\n[Visit website](https://app.magnumwallet.co)', true)
       .addField('Nanollet', '_Windows, Mac, Linux_\n[Visit website](https://nanollet.org)', true)
