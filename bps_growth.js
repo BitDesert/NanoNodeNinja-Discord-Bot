574bdd16b552b1340ff3b3e405e4c9d5596a2c6e
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-05-23 21:28:53 +0200

    better url

--- bps_growth.js
+++ bps_growth.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const trend = require("./trend");
 
 async function getBlocksPerSecondGrowth(dimension){
-  var stats = await axios.get('https://node-proxy.nanoticker.info/api/v1/data?chart=repstats_v21_local.tps_median&format=json&points=320&group=average&after=-3600&dimension='+dimension+'&options=flip|jsonwrap');
+  var stats = await axios.get('https://node-proxy.nanoticker.info/api/v1/data?chart=repstats_v21_local.tps_median&format=json&points=320&group=average&after=-3600&options=flip|jsonwrap&dimension='+dimension);
 
   var statsvalues = stats.data.result.data;
   let cps = statsvalues.map(a => a[1]);
