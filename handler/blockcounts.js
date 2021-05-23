f3f1990ce50b8812a70cf8f14f7679690d049b54
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-05-23 22:15:18 +0200

    add backlog

--- handler/blockcounts.js
+++ handler/blockcounts.js
@@ -16,6 +16,7 @@ async function sendBlockCounts(client, channel){
     .addField('Checked', parseInt(blocks.data.count).toLocaleString('en-US'), true)
     .addField('Unchecked', parseInt(blocks.data.unchecked).toLocaleString('en-US'), true)
     .addField('Cemented', parseInt(blocks.data.cemented).toLocaleString('en-US'), true)
+    .addField('Backlog', (parseInt(blocks.data.count) - parseInt(blocks.data.cemented)).toLocaleString('en-US'), true)
     .setFooter('My Nano Ninja | mynano.ninja', client.user.avatarURL)
 
   // Send the embed to the same channel as the message
